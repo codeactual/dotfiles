8a796c5c5beda84ff768e0c4d9202dd2f734c692
David Smith <codeactual@gmail.com>
2012-04-27 06:17:52 +0000

    Update git aliases.

--- zsh/vcs.zsh
+++ zsh/vcs.zsh
@@ -10,18 +10,21 @@ alias ga='git add'
 alias gap='git add --patch'
 alias gb='git branch'
 alias gba='git branch -a'                                                         # Include remote-tracking and local.
-alias gc='git commit -v'                                                          # Verbose commit.
+alias gco='git checkout'
+alias gci='git commit -v'                                                          # Verbose commit.
 alias gcatchup="git fetch && git rebase origin/master"                            # Catch up local snapshot of origin/master. But don't modify local master.
-alias gcp='git checkout --patch'
+alias gcop='git checkout --patch'
 alias gd='git diff'
 alias gda="git diff; git diff --cached"
 alias gdv='git diff | $EDITOR -R - '
-alias gl='git pull'
+alias gl='git log --oneline --graph --decorate'
 alias gld="git log -- \$1"                                                        # Search log for a deleted path.
 alias gmv='git mv'
 alias gp='git push'
-alias gs='git stash'
-alias gsa='git stash apply'                                                       # Like pop but w/out modifying stash list.
+alias gs='git status'
+alias gss='git status -sb'
+alias gsh='git stash'
+alias gsha='git stash apply'                                                       # Like pop but w/out modifying stash list.
 
 ################
 ##### SVN ######
