b39d68e623d93e0403e59e5646b1dbf8dfbe293e
David Smith <codeactual@gmail.com>
2012-05-04 23:46:25 +0000

    Add alias for Eclipse workspace.

--- zsh/android.zsh
+++ zsh/android.zsh
@@ -3,3 +3,4 @@ echo "Loading $0"
 alias adb-restart="adb kill-server && adb start-server"
 alias android-start-avd="emulator -verbose -avd" # android-start-avd <name>
 alias android-keygen-debug="keytool -exportcert -alias androiddebugkey -keystore ~/.android/debug.keystore | openssl sha1 -binary | openssl base64"
+alias cdworkspace="cd ~/workspace/"
