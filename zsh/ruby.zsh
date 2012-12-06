413d150c5588383f48ce864b1a5cfb250ac77a9a
David Smith <codeactual@gmail.com>
2012-12-06 13:26:55 -0800

    ADD custom GEM_HOME

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -1,3 +1,7 @@
 echo "Loading $0"
 
+# To avoid gem installation that require sudo.
+# Based on current "Installing RubyGems in a User Directory" http://docs.rubygems.org/
+export GEM_HOME="~/gems"
+
 alias ruby-gem-path="ruby -r rubygems -e 'p Gem.path'"
