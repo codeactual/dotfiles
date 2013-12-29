83cfa00e5d8dea732a09767fbad7ffbb806e351d
David Smith <codeactual@gmail.com>
2013-12-29 05:16:51 +0000

    feat(zsh/docker): Add dkrmold alias

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -14,6 +14,9 @@ alias dklc="docker ps -l -q"
 alias dkps="docker ps -a -s"
 alias dkr="docker run"
 
+# Delete the oldest 20 containers that are at least 2 hours old.
+alias dkrmold="docker ps -a | egrep 'day|week|hours' | tail -n 20 | awk '{print \$1}' | xargs docker rm; docker ps -a"
+
 LOCAL_ZSH_DIR=$HOME/docker/zsh
 if [ -d $LOCAL_ZSH_DIR ]; then
   for file in $LOCAL_ZSH_DIR/*.zsh
