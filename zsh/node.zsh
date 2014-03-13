29ad74ba37d30735c0492a8a5a0ffab20962ceb9
David Smith <codeactual@gmail.com>
2014-03-13 20:13:02 +0000

    fix(zsh/node): Remove env specific exports
    Move to per-env local versions.

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -1,10 +1,4 @@
 echo "Loading $0"
 
-NODE_MODULES_CWD_BIN_PATH=./node_modules/.bin
-NODE_MODULES_HOME_BIN_PATH=$HOME/bin/node_modules/.bin
-NODE_MODULES_GLOBAL_BIN_PATH=/usr/local/node/bin
-
-export PATH=$PATH:$NODE_MODULES_CWD_BIN_PATH:$NODE_MODULES_HOME_BIN_PATH:$NODE_MODULES_GLOBAL_BIN_PATH
-
 # https://twitter.com/thl0/status/347923557508063235
 alias lnpm='npm --no-registry'
