324610f90cabab7234d2e49989aef41a3666ffc0
David Smith <codeactual@gmail.com>
2014-04-10 23:37:33 +0000

    fix(zsh/ruby): Add GEM_PATH

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -4,7 +4,7 @@ echo "Loading $0"
 # See "Installing RubyGems in a User Directory" http://docs.rubygems.org/
 export RUBY_PREFIX="$HOME/opt/ruby"
 export GEM_HOME="$RUBY_PREFIX/gems"
-
+export GEM_PATH=$GEM_HOME
 export PATH=$PATH:$GEM_HOME/bin
 
 alias ruby-gem-path="ruby -r rubygems -e 'p Gem.path'"
