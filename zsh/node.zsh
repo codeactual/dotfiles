9d9a8a789d71741ead6ae528582baf56f80e8ebe
David Smith <codeactual@gmail.com>
2015-03-03 00:01:07 +0000

    feat(zsh/node): Init ganodemodules

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -2,3 +2,10 @@ echo "Loading $0"
 
 # https://twitter.com/thl0/status/347923557508063235
 alias lnpm='npm --no-registry'
+
+function ganodemodules {
+  local EXT=".*\.\(yml\|ttf\|md\|html\|css\|.*ignore\|gitmodules\)$"
+  local DIRS=".*/\(tests?\|examples?\|specs?\|fonts?\|docs?\)/.*"
+  local EXACT=".*/\(Gruntfile.*\|Makefile\|test.js\)$"
+  find ./node_modules -type f -not -regex $EXT -not -iregex $DIRS -not -iregex $EXACT -exec git add {} \;
+}
