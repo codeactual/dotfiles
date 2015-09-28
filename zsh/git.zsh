318f417e5426fbb4bc1803943e7aace8c1ee3a89
David Smith <codeactual@gmail.com>
2015-09-28 00:07:02 +0000

    feat(zsh/git): clear the screen before showing diffs

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -154,5 +154,10 @@ rule () {
 }
 
 function gd () {
-  git diff --color $@ | diff-highlight | strip_diff_leading_symbols
+  # Make it easier to see the latest diff (ex. if scrollback is already full of them) w/out searching for the prompt
+  # or some other marker. But only do so when inside tmux because scrollback loss isn't as much of an issue.
+  if [ "$TMUX" ]; then
+    clear
+  fi
+  git diff --color $@ | diff-highlight | strip_diff_leading_symbols | less --raw-control-chars --quit-if-one-screen --no-init
 }
