148284bc007c90417090e7ce912c18c23ddc10c7
David Smith <codeactual@gmail.com>
2016-08-11 17:34:17 +0000

    fix(docker.zsh): dkrmold/dkrmiold scope, remove unused

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,11 +1,5 @@
 echo "Loading $0"
 
-alias dk="docker"
-
-# -rm: Remove intermediate containers
-alias dkb="docker build"
-
-alias dkd="docker diff"
 alias dki="docker images"
 alias dkia="docker images -a"
 alias dkiat="docker images -a -tree"
@@ -20,14 +14,10 @@ alias dkr="docker run --rm=true"
 alias dkrd="docker run -d"
 alias dkrlast="dkr \`dkilast\`"
 
-alias dkt="docker tag"
 alias dktlast="dkt \`dkilast\` \$1"
 
-# Delete the oldest 20 containers that are at least about 1 hour old.
-alias dkrmold="docker ps -a --filter=status=exited | egrep 'minute|day|week|hour' | tail -n 20 | awk '{print \$1}' | xargs docker rm; docker ps -a"
-
-# Delete the oldest 20 images that are at least about 1 hour old.
-alias dkrmiold="docker images --filter=dangling=true | egrep '<none>' | tail -n 20 | sort --reverse | awk '{print \$3}' | xargs docker rmi; docker images"
+alias dkrmold="docker ps --all --filter=status=exited --filter=status=dead | awk '{print \$1}' | grep -v CONTAINER | xargs docker rm"
+alias dkrmiold="dkrmold; docker images --filter=dangling=true | grep '<none>' | awk '{print \$3}' | xargs docker rmi; docker images"
 
 alias dk-master-install="docker -v; sudo service docker stop ; sudo cp \$(which docker) \$(which docker)_ ; sudo cp ./bundles/*-dev/binary/docker-*-dev $(which docker); sudo service docker start; docker -v"
 
