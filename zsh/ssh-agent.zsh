0532fecd48331211fe164ee496f5d1eb1291158c
David Smith <codeactual@gmail.com>
2016-08-15 21:23:35 +0000

    fix(tmux,zsh/ssh-agent): out-of-date agent socket

--- zsh/ssh-agent.zsh
+++ zsh/ssh-agent.zsh
@@ -1,69 +1,19 @@
-# Based on existing script but I can't find the original source.
+# From https://gist.github.com/martijnvermaat/8070533#gistcomment-1778689
 echo "Loading $0"
 
-function export_agent_meta {
-  local pid=$1
-  local sock=$2
+# Launch SSH agent if not running
+if ! ps aux |grep $(whoami) |grep ssh-agent |grep -v grep >/dev/null; then ssh-agent ; fi
 
-  echo "Agent pid $pid"
-  export SSH_AGENT_PID="$pid"
-
-  echo "Agent sock $sock"
-  export SSH_AUTH_SOCK="$sock"
+function ssh_agent_link_socket {
+  # Link the latest ssh-agent socket
+  ln -sf $(find /tmp -maxdepth 2 -type s -name "agent*" -user $USER -printf '%T@ %p\n' 2>/dev/null |sort -n|tail -1|cut -d' ' -f2) ~/.ssh/ssh_auth_sock
 }
 
-function start_agent {
-  # this seems to be the proper method as opposed to `exec ssh-agent bash`
-  eval "$(ssh-agent)"
+export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+
+ssh_agent_link_socket
+ssh-add -l
+if [ $? -ne 0 ]; then
   ssh-add
-}
-
-function find_agent_meta {
-  # Check to see if SSH Agent is already running
-  agent_pid="$(ps -ef | grep "ssh-agent" | grep -v "grep" | awk '{print($2)}' | head -1)"
-  if [[ -z "$agent_pid" ]]; then
-    echo "No agent found"
-  else
-    agent_ppid=$((agent_pid - 1))
-    # and the actual auth socket file name is simply numerically one less than
-    # the actual process id, regardless of what `ps -ef` reports as the ppid
-    agent_sock="$(find /tmp -path "*ssh*" -type s -iname "agent.$agent_ppid" 2>&1 | grep -v 'Permission denied')"
-  fi
-}
-
-if dk-detect; then
-  echo "Possibly inside a docker container. Exiting $0."
-else
-  find_agent_meta
-
-  # If the agent is not running (pid is zero length string)
-  if [[ -z "$agent_pid" ]]; then
-      start_agent
-
-  # If the agent is running (pid is non zero)
-  else
-      # Connect to the currently running ssh-agent
-
-      # this doesn't work because for some reason the ppid is 1 both when
-      # starting from ~/.profile and when executing as `ssh-agent bash`
-      #agent_ppid="$(ps -ef | grep "ssh-agent" | grep -v "grep" | awk '{print($3)}')"
-      agent_ppid=$((agent_pid - 1))
-      echo "Agent ppid $agent_ppid"
-
-      # and the actual auth socket file name is simply numerically one less than
-      # the actual process id, regardless of what `ps -ef` reports as the ppid
-      agent_sock="$(find /tmp -path "*ssh*" -type s -iname "agent.$agent_ppid" 2>&1 | grep -v 'Permission denied')"
-
-      # Invalidate the agent PID if no credentials are attached
-      export_agent_meta $agent_pid $agent_sock
-      ssh-add -l
-      if [[ "$?" = 2 ]]; then
-        killall -9 ssh-agent
-        agent_pid=""
-        echo "Existing agent had no credentials"
-        start_agent
-        find_agent_meta
-        export_agent_meta $agent_pid $agent_sock
-      fi
-  fi
+  ssh_agent_link_socket
 fi
