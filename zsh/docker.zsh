6efaacc0eb4758f23311a0cd8129a1e40aa11bc9
David Smith <codeactual@gmail.com>
2016-08-21 22:08:11 +0000

    chore(zsh/docker): centralize in another repo

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,24 +1,3 @@
-alias dki="docker images"
-alias dkia="docker images -a"
-alias dkiat="docker images -a -tree"
-alias dkilast="docker images | grep '<none>' | head -1 | awk '{print \$3}'"
-
-# ID of last container
-alias dklc="docker ps -l -q"
-
-alias dkps="docker ps"
-alias dkpsa="docker ps -a --no-trunc=true"
-alias dkr="docker run --rm=true"
-alias dkrd="docker run -d"
-alias dkrlast="dkr \`dkilast\`"
-
-alias dktlast="dkt \`dkilast\` \$1"
-
-alias dk-master-install="docker -v; sudo service docker stop ; sudo cp \$(which docker) \$(which docker)_ ; sudo cp ./bundles/*-dev/binary/docker-*-dev $(which docker); sudo service docker start; docker -v"
-
-# http://stackoverflow.com/questions/20010199/determining-if-a-process-runs-inside-lxc-docker
-alias dk-detect="grep docker /proc/1/cgroup 2>&1 > /dev/null"
-
 LOCAL_ZSH_DIR=$HOME/zsh/docker
 if [ -d $LOCAL_ZSH_DIR ]; then
   for file in $LOCAL_ZSH_DIR/*.*sh
@@ -26,5 +5,3 @@ if [ -d $LOCAL_ZSH_DIR ]; then
     source $file
   done
 fi
-
-PATH=$PATH:$HOME/docker/bin
